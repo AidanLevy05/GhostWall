188e7659fc46f95f68b36391a060cb0091e54c49
Aidan Levy <aidanlevy05@gmail.com>
2026-02-28 22:49:37 -0500

    run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo DEFENSE_MODE=auto-block   DEFENSE_FIREWALL_BACKEND=nftables   FSSH_WHITELIST="172.20.10.3,172.20.10.2,127.0.0.1"   venv/bin/python3 TUI/tui.py --interface wlp0s20f3 --listen-port 22 --real-ssh-port 47832 --cowrie-port 2222
